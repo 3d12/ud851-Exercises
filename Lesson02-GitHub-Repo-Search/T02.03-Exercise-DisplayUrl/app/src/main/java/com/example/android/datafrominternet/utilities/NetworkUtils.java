c1f1bbfe42a93de400a96ffe54cf37a72a03e658
Nick E <3d12@users.noreply.github.com>
2018-01-17 16:18:40 -0600

    Completed T02.03

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,9 +15,14 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -26,17 +31,17 @@ import java.util.Scanner;
  */
 public class NetworkUtils {
 
-    final static String GITHUB_BASE_URL =
+    private final static String GITHUB_BASE_URL =
             "https://api.github.com/search/repositories";
 
-    final static String PARAM_QUERY = "q";
+    private final static String PARAM_QUERY = "q";
 
     /*
      * The sort field. One of stars, forks, or updated.
      * Default: results are sorted by best match if no field is specified.
      */
-    final static String PARAM_SORT = "sort";
-    final static String sortBy = "stars";
+    private final static String PARAM_SORT = "sort";
+    private final static String sortBy = "stars";
 
     /**
      * Builds the URL used to query Github.
@@ -45,8 +50,16 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+        // DONE (1) Fill in this method to build the proper Github query URL
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL);
+        builtUri = builtUri.buildUpon().appendQueryParameter(PARAM_QUERY, githubSearchQuery).appendQueryParameter(PARAM_SORT, sortBy).build();
+        URL newURL = null;
+        try {
+            newURL = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return newURL;
     }
 
     /**
