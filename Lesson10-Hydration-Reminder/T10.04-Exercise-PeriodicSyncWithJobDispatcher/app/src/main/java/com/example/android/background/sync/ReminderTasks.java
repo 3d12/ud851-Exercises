ecbdf317f457185d6b301763aa09c8f6a7526093
Nick E <3d12@users.noreply.github.com>
2018-04-08 21:30:19 -0500

    T10.04 Completed I'll be honest -- beyond the highest of the high-level overviews of the objective of this exercise, I'm still very confused as to what we did here. I know we scheduled the job, and it uses a FirebaseJobDispatcher to schedule it, but the implementation details are pretty lost on me. This may be an exercise I have to come back and review in more detail.

--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -16,20 +16,27 @@
 package com.example.android.background.sync;
 
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 
 import com.example.android.background.utilities.NotificationUtils;
 import com.example.android.background.utilities.PreferenceUtilities;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.firebase.jobdispatcher.SimpleJobService;
 
 public class ReminderTasks {
 
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
     public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
+    public static final String ACTION_CHARGING_REMINDER = "charging-reminder-notification";
 
     public static void executeTask(Context context, String action) {
         if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
             incrementWaterCount(context);
         } else if (ACTION_DISMISS_NOTIFICATION.equals(action)) {
             NotificationUtils.clearAllNotifications(context);
+        } else if (ACTION_CHARGING_REMINDER.equals(action)) {
+            chargingReminderNotification(context);
         }
     }
 
@@ -38,10 +45,14 @@ public class ReminderTasks {
         NotificationUtils.clearAllNotifications(context);
     }
 
-    // TODO (2) Create an additional task for issuing a charging reminder notification.
+    // DONE (2) Create an additional task for issuing a charging reminder notification.
     // This should be done in a similar way to how you have an action for incrementingWaterCount
     // and dismissing notifications. This task should both create a notification AND
     // increment the charging reminder count (hint: there is a method for this in PreferenceUtilities)
     // When finished, you should be able to call executeTask with the correct parameters to execute
     // this task. Don't forget to add the code to executeTask which actually calls your new task!
+    private static void chargingReminderNotification(Context context) {
+        NotificationUtils.remindUserBecauseCharging(context);
+        PreferenceUtilities.incrementChargingReminderCount(context);
+    }
 }
